a3490c9931d6f068ab7945661ab9abba6a0509b0
AmanTesting <139746798+DimiTesting@users.noreply.github.com>
2023-11-04 20:23:23 +0200

    Update main.py
    Removed secret Key

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from forms import AddCafeForm
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
